242fa4408e377d8f0311945ec1e609a8035aded9
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-02-03 23:19:28 +0000

    refactor: removing app file for now

--- personal-website/src/index.js
+++ personal-website/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AboutMePage from './aboutMe/aboutMePage';
 import CodingClubPage from './codingClub/CodingClubPage';
 import ExtraCurricularPage from './extraCurricular/ExtraCurricularPage';
 import MinorProjectsPage from './minorProjects/MinorProjectsPage';
 import MajorProjectsPage from './majorProjects/MajorProjectsPage';
+import './styles.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -22,8 +22,6 @@ root.render(
             <Route path="/MinorProjects"  element={<MinorProjectsPage />}/>
             <Route path="/MajorProjects"  element={<MajorProjectsPage />}/>
         </Routes>
-
-        <App />
 
       </BrowserRouter>
   </React.StrictMode>
